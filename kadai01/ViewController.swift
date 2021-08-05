880aeefbf114d36cc6ce27e3f90be91115d0847e
taiki-dev <taiki.murakoshi.0908@gmail.com>
2021-08-05 19:45:19 +0900

    first commit

---
+++ kadai01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai01
+//
+//  Created by Murakoshi Taiki on 2021/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
