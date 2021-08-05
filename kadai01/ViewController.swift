ad291ca4cb1959d58a44a24d7954cb82a880afe1
taiki-dev <taiki.murakoshi.0908@gmail.com>
2021-08-05 19:53:14 +0900

    課題01の回答

--- kadai01/ViewController.swift
+++ kadai01/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        func isLeapYear(year: Int) -> Bool {
+
+            let isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+
+            if isLeapYear{
+                return true
+            }else{
+                return false
+            }
+        }
+
+        print(isLeapYear(year: 2000))  // true
+        print(isLeapYear(year: 1209))  // false
+        print(isLeapYear(year: 1980))  // true
+        print(isLeapYear(year: 1790))  // false
+        print(isLeapYear(year: 1993))  // false
     }
 
 
